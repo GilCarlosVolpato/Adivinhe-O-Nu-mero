2bee931aaabdd5a68a10627905cc07d0b64f789c
Gil Carlos Volpato <gilcarlosvolpato@gmail.com>
2022-01-21 22:29:59 -0300

    Initial Commit

---
+++ Adivinhe O Número/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Adivinhe O Número
+//
+//  Created by Gil Carlos Volpato on 21/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
